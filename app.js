d6aa3798003a96b1fc78180b2c711ba820237db1
Anthony Cantellano <acantellano1996@gmail.com>
2018-11-08 01:32:27 -0800

    Made clear where api key goes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // if you haven't already, install the SDK with "npm install sightengine --save"
-var sightengine = require('sightengine')();
+var sightengine = require('sightengine')('{api_user}', '{api_secret}');
 sightengine.check(['nudity', 'wad', 'properties', 'celebrities', 'offensive', 'faces', 'scam', 'face-attributes', 'text']).set_url('https://media2.fdncms.com/clevescene/imager/rapper-ice-cube-discusses-nwas-legacy/u/original/3587882/1371843424-icecube3.jpg').then(function (result) {
     // The result of the API
     console.log(result)
