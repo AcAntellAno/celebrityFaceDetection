679ce939c45dc0cd056dce4d149a4581d0fe5365
Anthony Cantellano <AcAntellAno@users.noreply.github.com>
2018-12-01 22:01:34 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// if you haven't already, install the SDK with "npm install sightengine --save"
-var sightengine = require('sightengine')('{api_user}', '{api_secret}');
+var sightengine = require('sightengine')('{api_user}', '{api_secret}'); //note {} need to be deleted when you put your credentials
 sightengine.check(['nudity', 'wad', 'properties', 'celebrities', 'offensive', 'faces', 'scam', 'face-attributes', 'text']).set_url('https://media2.fdncms.com/clevescene/imager/rapper-ice-cube-discusses-nwas-legacy/u/original/3587882/1371843424-icecube3.jpg').then(function (result) {
     // The result of the API
     console.log(result)
